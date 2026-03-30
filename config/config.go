210d5d9448930010e7ae984c2c12f294b7bad4d1
0xYeah <wmyeah017@gmail.com>
2026-03-30 20:35:28 +0800

    Release:--: v0.0.3_2026-03-30_12:35:28_UTC

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	ProjectName     = "project_template_go"
-	ProjectVersion  = "v0.0.2"
+	ProjectVersion  = "v0.0.3"
 	ProjectBundleID = "com.project_template_go.project_template_go"
 	apiPortDefault  = 12095
 )
